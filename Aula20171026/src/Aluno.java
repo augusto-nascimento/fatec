20a04875fe54a5849ad3836405816ed31cf5e375
Augusto <augusto.r.nascimento@gmail.com>
2017-11-19 07:40:34 -0200

    Sistema Aluno

---
+++ Aula20171026/src/Aluno.java
@@ -0,0 +1,14 @@
+class Aluno {	 	 
+ String pnome;	 
+ String unome;	 
+ int    pontos;	 
+	 
+ // construtor	 
+ Aluno () {	 
+  this("" ,"" ,0);  }
+  Aluno ( String pnomealuno, String unomealuno,	 int pontosaluno )  {	 
+  pnome   = pnomealuno;	 
+  unome  = unomealuno;	 
+  pontos  = pontosaluno;	 
+ }	
+}	
---
+++ Aula20171026/src/ClassePrincipal.java
@@ -0,0 +1,24 @@
+/**
+ * *
+ *
+ * RICARDO SATOSHI
+ * 05/10/2016
+ */
+import java.io.IOException;
+import javax.swing.JOptionPane;
+public class ClassePrincipal {
+    public static void main (String arg [ ]) throws IOException {
+    Aluno[ ] aluno = new Aluno[3];	
+     ClasseMetodos m = new ClasseMetodos(); 
+      int opc=0      ;
+      while (opc!=9){
+ opc = Integer.parseInt(JOptionPane.showInputDialog("1 - Gravar Aluno \n 2 - Ler Aluno \n 9 - Finaliza"));
+      switch (opc)     {
+            case 1: aluno = m.GravaAluno(aluno);
+                         break;
+            case 2: m.LerAluno (aluno);
+                         break;
+            case 9: JOptionPane.showMessageDialog(null,"FINALIZADO ");
+                         break;
+            default: JOptionPane.showMessageDialog(null,"OPÇÃO INVÁLIDA");
+         }   }  } }
