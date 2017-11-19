86cfd8c3acd17a11fa2134029e0ddd5f5c239191
Augusto Rocha Nascimento <augusto.r.nascimento@gmail.com>
2017-11-19 08:59:35 -0200

    modificado metodo para gravacao no txt

--- Aula20171026/src/Aluno.java
+++ Aula20171026/src/Aluno.java
@@ -2,13 +2,13 @@ class Aluno {
  String pnome;	 
  String unome;	 
  int    pontos;	 
-	 
+ 
  // construtor	 
  Aluno () {	 
   this("" ,"" ,0);  }
   Aluno ( String pnomealuno, String unomealuno,	 int pontosaluno )  {	 
-  pnome   = pnomealuno;	 
-  unome  = unomealuno;	 
-  pontos  = pontosaluno;	 
+  pnome   = pnomealuno;
+  unome  = unomealuno;
+  pontos  = pontosaluno;
  }	
 }	
--- Aula20171026/src/ClasseMetodos.java
+++ Aula20171026/src/ClasseMetodos.java
@@ -1,42 +1,47 @@
 
 import java.io.*;	
-import javax.swing.*;	
+import javax.swing.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 public class ClasseMetodos {
- public Aluno[ ] GravaAluno (Aluno[ ] aluno  ) throws IOException {	
-     int i;	
-     String fileName = "ArquivoAluno.txt";	
-     BufferedWriter writer = new BufferedWriter(new FileWriter( fileName ));	
-     for (i = 0 ; i < 3 ; i++)	
-          aluno[i] = new Aluno();
-	
-     for (i = 0 ; i < 3 ; i++)  {	
-           aluno[i].pnome = JOptionPane.showInputDialog("Entre o nome do aluno:");	
-           writer.write( aluno[i].pnome );  	
-           writer.newLine();	
-           aluno[i].unome = JOptionPane.showInputDialog("Entre o sobrenome do aluno:");	
-           writer.write( aluno[i].unome );  	
-           writer.newLine();	
-           aluno[i].pontos = Integer.parseInt(JOptionPane.showInputDialog("Pontos Graduação do aluno:"));	
-           writer.write(Integer.toString(aluno[i].pontos));   	
-          writer.newLine();  } 
-      System.out.println("GRAVAÇÃO FEITA COM SUCESSO ");	
-    writer.close();
+ public Aluno GravaAluno (Aluno aluno) throws IOException {	
+    int i;
+    String row_record;
+    String fileName = "ArquivoAluno2.txt";
+    aluno = new Aluno();
+    aluno.pnome = JOptionPane.showInputDialog("Entre o nome do aluno:");	
+    aluno.unome = JOptionPane.showInputDialog("Entre o sobrenome do aluno:");	
+    aluno.pontos = Integer.parseInt(JOptionPane.showInputDialog("Pontos Graduação do aluno:"));
+    row_record = String.format("%s|%s|%s\n", aluno.pnome, aluno.unome, aluno.pontos);
+    Path path = Paths.get(fileName);
+    if (Files.exists(path)) {
+        Files.write(path, row_record.getBytes(), StandardOpenOption.APPEND);
+    }
+    else {
+        File f = new File(fileName);
+        f.createNewFile();
+        Files.write(path, row_record.getBytes(), StandardOpenOption.APPEND);
+    }
+    System.out.println("GRAVAÇÃO FEITA COM SUCESSO ");	
+//    writer.close();
   return aluno;
-  }	
+}	
 
 
 	
- public void LerAluno ( Aluno[ ] aluno ) throws IOException	 {	
-   int i;	
-  String fileName = "ArquivoAluno.txt";	
-  BufferedReader ler = new BufferedReader(new FileReader( fileName ));	
-  for (i = 0 ; i < 3 ; i++)	
-   aluno[i] = new Aluno();
-  for (i = 0 ; i < 3 ; i++)   {	
-    aluno[i].pnome = ler.readLine();  	
-    aluno[i].unome = ler.readLine();	
-    aluno[i].pontos = Integer.parseInt(ler.readLine());	
-   }	
+ public void LerAluno ( Aluno[ ] aluno ) throws IOException{
+    int i;
+    String fileName = "ArquivoAluno.txt";	
+    BufferedReader ler = new BufferedReader(new FileReader( fileName ));	
+    for (i = 0 ; i < 3 ; i++)	
+        aluno[i] = new Aluno();
+        for (i = 0 ; i < 3 ; i++)   {	
+            aluno[i].pnome = ler.readLine();
+            aluno[i].unome = ler.readLine();
+            aluno[i].pontos = Integer.parseInt(ler.readLine());	
+        }
   	
   for (i = 0 ; i < 3; i++) {	
    System.out.println(aluno[i].pnome + " " + aluno[i].unome + " pontos: " + aluno[i].pontos);	
